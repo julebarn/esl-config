65f490117b5028aa6cbe0a11ec0276b767e9282d
mads jerup <madsjerup@gmail.com>
2021-07-10 22:57:56 +0200

    add "no-unsafe-optional-chaining": "error"

--- index.js
+++ index.js
@@ -50,5 +50,6 @@ module.exports = {
         "no-unreachable-loop": "error",
         "no-unsafe-finally": "error",
         "no-unsafe-negation": "error",
+        "no-unsafe-optional-chaining": "error",
     }
 };
