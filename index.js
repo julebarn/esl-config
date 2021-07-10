c3999f8b9b9dc80d5b65280e82570eed1a87e177
mads jerup <madsjerup@gmail.com>
2021-07-10 21:38:34 +0200

    add  "no-dupe-args": "error"

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ module.exports = {
         "no-constant-condition": ["error", { "checkLoops": false }],
         "no-control-regex": "error",
         "no-debugger": "error",
+        "no-dupe-args": "error",
         "no-dupe-else-if": "error",
     }
 };
