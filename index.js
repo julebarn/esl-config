132d11a1a4c5c7ea542bb8aae9a2aadc9c141bec
mads jerup <madsjerup@gmail.com>
2021-07-12 20:17:33 +0200

    removed object-curly-newline.minProperties

--- index.js
+++ index.js
@@ -289,7 +289,6 @@ module.exports = {
         "no-whitespace-before-property": "error",
         "nonblock-statement-body-position": "off",
         "object-curly-newline": ["error", {
-            "minProperties": 2,
             "consistent": true,
         }],
         "object-curly-spacing": ["error", "always"],
